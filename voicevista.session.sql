2bad32f2d5bbbe52672740f79cc83bb7474c2608
Strawbang <dbougouffa@gmail.com>
2023-11-19 19:23:31 +0100

    update

---
+++ voicevista.session.sql
@@ -0,0 +1,23 @@
+CREATE KEYSPACE IF NOT EXISTS voicevista 
+WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : '1' };
+CREATE TABLE IF NOT EXISTS voicevista.servers (
+    serverid BIGINT PRIMARY KEY,
+    name text,
+    categoryLists list<BIGINT>,
+    channelLists list<BIGINT>
+);
+CREATE TABLE IF NOT EXISTS voicevista.messages (
+    messageid BIGINT,
+    content text,
+    user text,
+    channelid BIGINT,
+    PRIMARY KEY (messageid, channelid)
+);
+CREATE TABLE IF NOT EXISTS voicevista.categories (
+    categoryid BIGINT PRIMARY KEY,
+    name text
+);
+CREATE TABLE IF NOT EXISTS voicevista.channels (
+    channelid BIGINT PRIMARY KEY,
+    name text
+);
